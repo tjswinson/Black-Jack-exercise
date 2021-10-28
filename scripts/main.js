723f17b50e39a5a68c213919dad86a2365f3fcb0
tjswinson <tiffanyjswinson@gmail.com>
2021-10-28 19:32:47 -0400

    Add event listener

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 window.addEventListener('DOMContentLoaded', function() {
   // Execute after page load
+  const dealBtn = document.querySelector("#deal-button");
+  dealBtn.addEventListener("click", function(){
+    console.log("Hello World");
+
+    // create a new card image element
+    const newCard = document.createElement('img');
+    console.log(newCard);
+  });
+
 })
+
+
